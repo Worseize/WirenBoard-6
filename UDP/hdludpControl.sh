cdfb1e6ed9c24be6a02417331aef29434c205818
Nikita Bogdanov <worseize@gmail.com>
2022-02-21 15:52:49 +0300

    Delete UDP directory

--- UDP/hdludpControl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-message=$(split -b 2 --filter='cat;echo -n "\x"' <<< 'rrc0a80ae7534d415254434c4f5544aaaa'"$1" | sed 's/^..//;s/..$//')
-ip="192.168.10.100"
-port="6000"
-echo -e -n "$message" | nc -u -w1 "$ip" "$port"
--- UDP/hdludpFeedback.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while : ;do socat -T1 -u udp-recvfrom:6000 exec:"/usr/bin/xxd -p - /var/udp/streamingData.txt";msg=$(cat /var/udp/streamingData.txt | cut -b33-99);mosquitto_pub -t "/devices/hdl-listen/controls/data/on" -m "$msg";done;
