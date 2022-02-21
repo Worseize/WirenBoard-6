299d9ec84ea1318b7246543d7a5be419ddf5cc91
Nikita Bogdanov <worseize@gmail.com>
2022-02-21 15:38:19 +0300

    Create smartbusControl.sh
    wirenboard broadcast data via UDP to all smartbus devices

---
+++ SmartBus/smartbusControl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+message=$(split -b 2 --filter='cat;echo -n "\x"' <<< 'rrc0a80ae7534d415254434c4f5544aaaa'"$1" | sed 's/^..//;s/..$//')
+ip="192.168.10.100"
+port="6000"
+echo -e -n "$message" | nc -u -w2 "$ip" "$port"
