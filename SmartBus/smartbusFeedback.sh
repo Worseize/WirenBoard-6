19a57f08921b369c62747a776bea6b4cebc49273
Nikita Bogdanov <worseize@gmail.com>
2022-02-21 15:46:26 +0300

    Update smartbusFeedback.sh

--- SmartBus/smartbusFeedback.sh
+++ SmartBus/smartbusFeedback.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while : ;do socat -T1 -u udp-recvfrom:6000 exec:"/usr/bin/xxd -p - /var/udp/streamingData.txt";msg=$(cat /var/udp/streamingData.txt | cut -b33-99);mosquitto_pub -t "/devices/hdl-listen/controls/data/on" -m "$msg";done;
+while : ;do socat -T1 -u udp-recvfrom:6000 exec:"/usr/bin/xxd -p - /var/Smartbus/streamingData.txt";msg=$(cat /var/Smartbus/streamingData.txt | cut -b33-99);mosquitto_pub -t "/devices/hdl-listen/controls/data/on" -m "$msg";done;
