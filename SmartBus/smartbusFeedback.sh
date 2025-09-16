a0ac7ae935aabc06060eb77559833fc90c82d301
Nikita Bogdanov <worseize@gmail.com>
2025-09-16 15:27:27 +0300

    Update smartbusFeedback.sh

--- SmartBus/smartbusFeedback.sh
+++ SmartBus/smartbusFeedback.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-while : ;do socat -T1 -u udp-recvfrom:6000 exec:"/usr/bin/xxd -p - /mnt/Smartbus/streamingData.txt";msg=$(cat /mnt/Smartbus/streamingData.txt | cut -b33-99);mosquitto_pub -t "/devices/hdl-listen/controls/data/on" -m "$msg";done;
+
+# Используем socat с fork для параллельной обработки
+exec socat -T1 -u UDP-RECVFROM:6000,fork SYSTEM:'
+    # Конвертируем данные в hex
+    data=$(cat - | xxd -p)
+    
+    # Извлекаем нужную часть
+    msg=$(echo -n "$data" | cut -b33-99)
+    
+    # Отправляем в MQTT если сообщение не пустое
+    if [ -n "$msg" ]; then
+        # Лимит времени отправки (5 секунд)
+        timeout 5 mosquitto_pub -t "/devices/hdl-listen/controls/data/on" -m "$msg" || \
+        echo "$(date): MQTT timeout: $msg" >> /var/log/smartbus.log
+    fi
+'
